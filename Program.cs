2476b753be2ed680069a8abf7172de0d9aa563f4
Ferhan Abaci <ferhanabacii@gmail.com>
2022-02-17 19:57:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+           // int deger = 2;
+           // string degisken = null;
+           // string Degisken = null;
+           byte b = 5;
+           sbyte c = 5;
+           short s = 5;
+           ushort us = 5;
+
+            long l= 4;
+            ulong ul = 4;
+
+            float f= 5 ;
+            double d= 5;
+            decimal de=  5;
+
+            char ch = '2';
+            string str = "sınırsız";
+            bool b1 = true;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            string str1 = string.Empty;
+            str1= "Ferhan Abacı ";
+            string ad ="Zikriye ";
+            string soyad= "Ürkmez ";
+            string tisim = ad +""+soyad;
+
+
+            
+
+        }
+    }
+}
